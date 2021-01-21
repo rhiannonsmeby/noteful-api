b0ccd8d49b7e8eae2645b4d9f5e4cc0c8d4caab2
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-21 13:24:45 -0600

    C:/Program Files/Git/

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('./app')
 const {PORT, DATABASE_URL} = require('./config')
 
-pg.defaults.ssl = process.env.NODE_ENV === 'production';
+// pg.defaults.ssl = process.env.NODE_ENV === 'production';
 
 const db = knex({
     client: 'pg',
