648dd4af1c34affb1c5122b0b46c566abae6b5ca
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-21 15:17:47 -0600

    .

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const knex = require('knex')
 const app = require('./app')
 const {PORT, DATABASE_URL} = require('./config')
 
-// pg.defaults.ssl = process.env.NODE_ENV === 'production';
-
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
@@ -13,5 +11,4 @@ app.set('db', db)
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
-    pg.defaults.ssl = process.env.NODE_ENV === 'production'
 })
