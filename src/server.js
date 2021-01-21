2ea8d4ae8a223cba2d46282f08e7329e8acf1861
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-21 11:06:09 -0600

    ready to deploy

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const knex = require('knex')
 const app = require('./app')
 const {PORT, DATABASE_URL} = require('./config')
 
+pg.defaults.ssl = process.env.NODE_ENV === 'production';
+
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
