6a4c1fbd56548b093a676a0e3256d9e3c0b6d154
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-21 13:19:48 -0600

    updated port

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
-    "ssl": process.env.NODE_ENV === 'production',
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
